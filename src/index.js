01b282d237f21f7e48cfddc8cda764acb5aa782c
ksenofonus <danilova.ka88@yandex.ru>
2023-08-09 20:33:54 +0400

    feat: add reverse-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const absNum = Math.abs(n);
+  const absNumToString = String(absNum);
+  let result = '';
+  for (let i = 0; i < absNumToString.length; i = i + 1) {
+    result = `${absNumToString[i]}${result}`;
+  }
+  return Number(result);
 }
